2a3d644c4d265a61d21adb45f7391637ed66f40e
bomtall <bomtall1@hotmail.com>
2022-12-23 11:55:01 +0000

    updating quiz json

--- script/quiz.js
+++ script/quiz.js
@@ -1,14 +1,27 @@
 
+// Define arrays to hold quiz data
+var questions = [];
+var options = [];
+var answers = [];
 
+// Get elements to put data into
+var optionlabels = document.getElementsByClassName("quizRadioLabels")
+var qlabels = document.getElementsByClassName("questionLabels")
+
+
+var getAnswers = false
+
+//Get the quiz data json object and pass to loadQuiz function
 const xmlhttp = new XMLHttpRequest();
 xmlhttp.onload = function () {
-    // var myObj = JSON.parse(this.responseText);
 
-    loadQuiz(JSON.parse(this.responseText));
+    if (getAnswers) {
+        markQuiz(JSON.parse(this.responseText));
+    } else {
+        loadQuiz(JSON.parse(this.responseText));
+    }
 
-    // console.log(myObj);
-    // console.log(myObj.q1.question);
-    // document.getElementById("q1_label").innerHTML = myObj.q1.question;
+
 
 };
 
@@ -16,13 +29,76 @@ xmlhttp.open("GET", "https://raw.githubusercontent.com/bomtall/site/main/json/qu
 xmlhttp.send();
 
 
+
+//Load the quiz questions and possible answers into the elements
 function loadQuiz(data) {
 
-    console.log(data.q2.answer);
-    document.getElementById("q1_label").innerHTML = data.q1.question;
-    document.getElementById("q2_label").innerHTML = data.q2.question;
+    for (const [key, value] of Object.entries(data)) {
+
+        console.log(key);
+        console.log(value);
+
+        questions.push(value.question);
+        answers.push(value.answer);
+
+        console.log(value.question);
+        console.log(value.answer);
+
+        console.log(value.options);
+
+        for (i = 0; i < value.options.length; i++) {
+            options.push(value.options[i])
+        }
+
+    }
+
+    for (let i = 0; i < qlabels.length; i++) {
+        qlabels[i].innerHTML = questions[i]
+    }
+
 
 }
 
+
+
+function loadAnswers() {
+    getAnswers = true;
+
+    xmlhttp.open("GET", "https://raw.githubusercontent.com/bomtall/site/main/json/quiz_data.json");
+    xmlhttp.send();
+}
+
+
+
+
+function markQuiz(data) {
+
+    let answers = []
+
+    for (const [key, value] of Object.entries(data)) {
+        console.log(`${key}: ${value}`);
+
+        console.log(value['real_answer']);
+
+        answers.push(value['real_answer'])
+
+
+    }
+
+    let score = 0
+
+    for (i = 0; i < answers.length; i++) {
+
+        //look up which answer is checked for each question
+        //compare to real answer in answers list above
+
+
+
+    }
+
+    console.log(answers)
+
+
+}
 
 
