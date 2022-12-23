daa95ad6695cc94cad56d0aa17507145095eb265
bomtall <bomtall1@hotmail.com>
2022-12-23 11:54:40 +0000

    updating quiz json

--- script/main_script.js
+++ script/main_script.js
@@ -23,27 +23,7 @@ function validateLogin() {
     }
 };
 
-const quiz_data = {
-    "question1": { "question": "What is 5 + 25?", "answer": 30 },
-    "question2": { "question": "What is 18/3?", "answer": 6 }
-
-}
-
-function markQuiz() {
-    console.log(quiz_data.question1.answer)
-    let answers = [undefined, 30, 6]
-    let score = 0
-    let username = document.forms["quiz"]["name"].value;
-    let userAns = document.forms["quiz"];
-
-    for (let i = 1; i < document.forms["quiz"].length - 1; i++) {
-        if (userAns[i].value == answers[i]) {
-            score += 1
-        }
-    }
-    document.getElementById("outputbox").innerHTML += (username + " your score is " + score);
 
 
 
-}
 
